28fd2d04b4074c2094ce594ca5702e8cd486c1b3
YH <csw03105@naver.com>
2026-06-25 22:18:33 +0900

    fix: 구글로그인 시큐리티 추가

--- backend/src/main/java/com/ssafy/mvc/config/SecurityConfig.java
+++ backend/src/main/java/com/ssafy/mvc/config/SecurityConfig.java
@@ -143,7 +143,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173", "http://localhost:5174"));
+        config.setAllowedOrigins(List.of("http://localhost:5173", "http://localhost:5174", frontendUrl));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
